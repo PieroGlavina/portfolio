873adcb1a2202011c1658956322a0b0df0713b74
PieroGlavina <1f.glavinapiero@gmail.com>
2025-07-25 18:59:17 +0200

    first video experiments

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import OceanScene from "./OceanScene.jsx";
+import React, {useRef} from 'react'
 
 const Hero = () => {
+
+    const videoRef = useRef(null);
+
+    const handleMouseEnter = () => {
+        if (videoRef.current) videoRef.current.playbackRate = 2;
+    };
+
+    const handleMouseLeave = () => {
+        if (videoRef.current) videoRef.current.playbackRate = 1;
+    };
+
+
     return (
-        <section className="h-screen w-full">
-            <OceanScene />
+        <section className="h-screen w-full bg-red-500">
+
+            <video
+                ref={videoRef}
+                className="cursor-pointer h-[50%] w-[50%]"
+                autoPlay
+                muted
+                playsInline
+                loop
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+                onLoadedMetadata={() => {
+                    if (videoRef.current) videoRef.current.playbackRate = 0.8;
+                }}            >
+                <source src="/video/output.webm" type="video/webm" />
+            </video>
+
 
         </section>
     )
